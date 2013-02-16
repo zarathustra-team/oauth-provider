092a4e641c2bdc43a506c77b1dd670ba63881faf
Lookis <lucas@diandian.com>
2013-02-16 20:53:33 +0800

    add seed for application

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,10 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Passport.create(email: 'lookisliu@gmail.com', password: 'abc12345678')
+
+application=Doorkeeper::Application.new
+application.name='TestApplication'
+application.uid='6b4f2479415b240380179b3aeb5fa93e6746a03afd9b81b1017dcb277d2ce9c7'
+application.secret='d9e9264ed34f9213d95833782a91b2d5f247c97113364d2637e589c4f968cacc'
+application.redirect_uri='urn:ietf:wg:oauth:2.0:oob'
+application.save!
